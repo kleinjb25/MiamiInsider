61e30b91721b130d51f316902bc1e2cc5693389d
Yusuf Ozdemir <ozdemiye@miamioh.edu>
2023-02-21 18:14:37 -0500

    Began work on models, some sample routes in init

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-# For specifying the app's port
 import os
 from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
 # All of the database related stuff is stored here, we are importing it all
@@ -6,19 +5,56 @@ from database.models import *
 
 app = Flask(__name__)
 
-# Conneceting the database to the Flask project
-db_filename = 'database.db'
+# Connecting the database to the Flask project
 project_dir = os.path.dirname(os.path.abspath(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(project_dir, db_filename))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(project_dir, 'database/database.db'))
 
 db.init_app(app)
+
+# Run ONLY ONCE the first time, what it does is create the tables
+#   inside the database.db file.
+'''
+with app.app_context():
+    db.create_all()
+'''
 
 # -----------------
 # PAGES BELOW
 # -----------------
+
+# Simply prints the list of current locations
 @app.route('/')
 def index():
-    return "hello, world"
+    return render_template("index.html", locations=Location.query.all())
+
+# Example of adding a location to the table
+@app.route('/post_loc', methods=['POST'])
+def post_loc():
+
+    new_loc = Location(
+        name=request.form['name'], 
+        address=request.form['address'], 
+        description=request.form['description'], 
+        contact_email=request.form['contact_email'], 
+        contact_phone=request.form['contact_phone'], 
+        num_reviews=0
+    )
+    
+    db.session.add(new_loc)
+    db.session.commit()
+
+    # Redirects to the home page
+    return redirect('/')
+
+# Example of deleting a location from the table
+@app.route('/del_loc/<int:id>', methods=['DELETE'])
+def del_loc(id: int):
+
+    del_loc = Location.query.filter_by(id=id).first()
+    db.session.delete(del_loc)
+    db.session.commit()
+
+    return redirect('/')
 
 
 # -----------------
--- database/models.py
+++ database/models.py
@@ -1,3 +1,59 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy#, Model, Column, Integer, String, Float, Date
 
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String, nullable=False)
+    num_reviews = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return "<User(id='%d', first_name='%s', last_name='%s', email='%s')>" % (
+            self.id,
+            self.first_name,
+            self.last_name,
+            self.email
+        )
+
+class Location(db.Model):
+    __tablename__ = "locations"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    address = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String, nullable=False)
+    contact_email = db.Column(db.String(60))
+    contact_phone = db.Column(db.String(60))
+    num_reviews = db.Column(db.Integer, nullable=False)
+    avg_rating = db.Column(db.Float)
+
+    def __repr__(self):
+        return "<Location(id='%d', name='%s', avg_rating='%s')>" % (
+            self.id,
+            self.name,
+            self.avg_rating,
+        )
+
+'''
+class Review(db.Model):
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column()  # Relational
+    location_id = db.Column()  # Relational
+    rating = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.String(1024))
+
+    def __repr__(self):
+        return "<Review(id='%d', user_id='%d', location_id='%d')>" % (
+            self.id,
+            self.user_id,
+            self.location_id,
+        )     
+'''
