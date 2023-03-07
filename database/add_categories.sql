2f08a3c96d7a9eb9e863e368dd6857ce7bf9d433
Yusuf Ozdemir <ozdemiye@miamioh.edu>
2023-03-06 21:54:27 -0500

    Added sql script to add sample categories

---
+++ database/add_categories.sql
@@ -0,0 +1,5 @@
+-- Adds some sample categories to the 'categories' table
+INSERT INTO categories VALUES (1, 'Restaurants', 'utensils');
+INSERT INTO categories VALUES (2, 'Clinics', 'suitcase-medical');
+INSERT INTO categories VALUES (3, 'Bars', 'beer-mug-empty');
+INSERT INTO categories VALUES (4, 'Hotels', 'hotel');
